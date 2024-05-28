2ffa50d45a88711347703b7211e63eab46f1e1b8
ALakade1 <ankitlakade007@gmail.com>
2024-05-28 13:13:04 +0530

    Adding beer Entity. closes #1

---
+++ src/main/java/com/restSpring/msscbrewery/domain/Beer.java
@@ -0,0 +1,56 @@
+package com.restSpring.msscbrewery.domain;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Beer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+	private UUID id;
+	
+	@Version
+	private Integer version;
+	
+	@CreationTimestamp
+	@Column(updatable = false)
+	private OffsetDateTime createdDate;
+	
+	@UpdateTimestamp
+	private OffsetDateTime lastModifiedDate;
+	
+	private String beerName;
+	private String beerStyle;
+	
+	@Column(unique = true)
+	private Long upc;
+	private BigDecimal price;
+	private Integer minOnHand;
+	
+	private Integer quantityToBrewss;
+}
