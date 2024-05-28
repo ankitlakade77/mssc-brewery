95b1862c88a91d42a7a3380a4d976503f421e4a6
ALakade1 <ankitlakade007@gmail.com>
2024-05-28 13:34:36 +0530

    Adding the bootstrap for beer. closes #3

---
+++ src/main/java/com/restSpring/msscbrewery/bootstrap/BeerLoader.java
@@ -0,0 +1,46 @@
+package com.restSpring.msscbrewery.bootstrap;
+
+import java.math.BigDecimal;
+
+import org.springframework.boot.CommandLineRunner;
+
+import com.restSpring.msscbrewery.domain.Beer;
+import com.restSpring.msscbrewery.repositories.BeerRepository;
+
+public class BeerLoader implements CommandLineRunner{
+	
+	private final BeerRepository beerRepository;
+
+	public BeerLoader(BeerRepository beerRepository) {
+		super();
+		this.beerRepository = beerRepository;
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+	
+	private void loadBeer() {
+		if(beerRepository.count() == 0) {
+			beerRepository.save(Beer.builder()
+					.beerName("Mango Bobs")
+					.beerStyle("IPA")
+					.quantityToBrewss(200)
+					.minOnHand(12)
+					.upc(33370100000231L)
+					.price(new BigDecimal("12.95"))
+					.build());
+			
+			beerRepository.save(Beer.builder()
+					.beerName("Galaxy Cat")
+					.beerStyle("PALE_ALE")
+					.quantityToBrewss(300)
+					.minOnHand(35)
+					.upc(33170200000245L)
+					.price(new BigDecimal("11.95"))
+					.build());
+		}
+	}
+
+}
--- src/main/java/com/restSpring/msscbrewery/repositories/BeerRepository.java
+++ src/main/java/com/restSpring/msscbrewery/repositories/BeerRepository.java
@@ -1,10 +1,10 @@
 package com.restSpring.msscbrewery.repositories;
 
 import java.util.UUID;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.restSpring.msscbrewery.domain.Beer;
 
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID>{
+public interface BeerRepository extends JpaRepository<Beer, UUID>{
 
 }
