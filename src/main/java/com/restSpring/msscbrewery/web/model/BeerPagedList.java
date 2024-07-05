77a43ab246ca2eb22e84ed3b79568e1cd7b95017
ALakade1 <ankitlakade007@gmail.com>
2024-07-05 18:07:05 +0530

    Adding Json Jackson creator for paged list. closes #7

--- src/main/java/com/restSpring/msscbrewery/web/model/BeerPagedList.java
+++ src/main/java/com/restSpring/msscbrewery/web/model/BeerPagedList.java
@@ -3,15 +3,34 @@ package com.restSpring.msscbrewery.web.model;
 import java.util.List;
 
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class BeerPagedList extends PageImpl<BeerDto> {
 
 	private static final long serialVersionUID = 1L;
 
+	@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+	public BeerPagedList(@JsonProperty("content")List<BeerDto> content,
+						@JsonProperty("number")int number,
+						@JsonProperty("size")int size,
+						@JsonProperty("totalElements")Long totalElements,
+						@JsonProperty("pageble")JsonNode pageble,
+						@JsonProperty("last")boolean last,
+						@JsonProperty("totalPages")Long totalPages,
+						@JsonProperty("sort")JsonNode sort,
+						@JsonProperty("first")boolean first,
+						@JsonProperty("numberOfElements")int numberOfElements) {
+		
+		super(content, PageRequest.of(number, size), totalElements);
+	}
+	
 	public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
 		super(content, pageable, total);
-		// TODO Auto-generated constructor stub
 	}
 
 	public BeerPagedList(List<BeerDto> content) {
